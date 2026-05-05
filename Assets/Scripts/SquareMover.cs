750641b870b419a3092ac9681a06a0d586391125
cowlingj <cowlingj@OA-S141-03>
2026-05-05 17:11:54 -0400

    bouncing square

---
+++ Assets/Scripts/SquareMover.cs
@@ -0,0 +1,44 @@
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class SquareMover : MonoBehaviour
+{
+    Vector3 newPosition;
+    public float speed = 0.03f;
+    public float maxSpeed = 1f;
+    public float acceleration = 0.01f;
+    float xMax = 8.4f;
+    float xMin = -8.4f;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        newPosition = transform.position;
+
+
+
+
+
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        
+        if(Mathf.Abs(speed) <= maxSpeed)
+        {
+           speed += acceleration; // Increase speed by acceleration
+        }
+
+        if (newPosition.x >= xMax || newPosition.x <= xMin)
+        {
+            speed *= -1f; 
+        }
+
+        newPosition.x += speed;
+        transform.position = newPosition; // Update the position of the GameObject
+
+    }
+}
